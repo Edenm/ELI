bc2a891ca4aa0fbb709fa4d2b0984c369e365434
margulishahar <margulis_shahar@walla.co.il>
2015-03-07 18:43:49 +0200

    add refernce class

---
+++ Eli/Models/Reference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Eli.Models
+{
+    public class Reference
+    {
+        public List<tblReference> r;
+        public List<tblTherapist> th;
+        public List<tblTreatment> tr;
+    }
+}
