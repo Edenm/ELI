ab3698d97044a3e653d9459ae4d482328d155805
margulishahar <margulis_shahar@walla.co.il>
2015-08-09 17:49:34 +0300

    add excel controler +patient query

---
+++ Eli/Controllers/ExcelController.cs
@@ -0,0 +1,60 @@
+﻿using Eli.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eli.Controllers
+{
+    public class ExcelController : Controller
+    {
+        //
+        // GET: /Excel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PatientExcel()
+        {
+
+            EliManagerDB db = new EliManagerDB();
+            List<tblPatient> patient = db.Patients.ToList();
+
+            var grid = new GridView();
+            grid.DataSource = from p in patient
+                              select new
+                              {
+                                  שם = p.FirstName+" "+p.SurName,
+                                   
+                                  טלפון=p.PhoneNumber
+                              };
+            grid.DataBind();
+
+            Response.ClearContent();
+            Response.Buffer = true;
+            Response.AddHeader("content-disposition", "attachment; filename=PatientList.xls");
+            Response.ContentType = "application/ms-excel";
+            Response.ContentEncoding = System.Text.Encoding.Default;
+
+
+
+            Response.Charset = "";
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+
+            grid.RenderControl(htw);
+
+            Response.Output.Write(sw.ToString());
+            Response.Flush();
+            Response.End();
+
+            return View();
+        }
+
+    }
+}
