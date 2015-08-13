b78caee6f217a790c7380e878bda7316675c3609
margulishahar <margulis_shahar@walla.co.il>
2015-08-13 18:51:27 +0300

    update

---
+++ Eli/Controllers/PdfController.cs
@@ -0,0 +1,31 @@
+﻿using Eli.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eli.Controllers
+{
+    public class PdfController : Controller
+    {
+        //
+        // GET: /Pdf/
+
+        public ActionResult Index()
+        {
+            EliManagerDB db = new EliManagerDB();
+
+            List<tblTherapist> t = db.Therapist.ToList();
+            return View(t);
+        }
+
+        public ActionResult GeneratePDF()
+        {
+            return new Rotativa.ActionAsPdf("Index");
+        }
+
+        
+
+    }
+}
