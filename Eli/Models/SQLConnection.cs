d01bbbd47cea34861ec69db55ffe05677947674b
margulishahar <margulis_shahar@walla.co.il>
2015-08-28 17:49:31 +0300

    patient report excel

--- Eli/Models/SQLConnection.cs
+++ Eli/Models/SQLConnection.cs
@@ -8,10 +8,10 @@ namespace Eli.Models
     public class SQLConnection
     {
         //for shahar
-        //private static string connectionString = "Data Source=SHAHAR-PC\\SQLEXPRESS;Initial Catalog=Eli;Integrated Security=True";
+        private static string connectionString = "Data Source=SHAHAR-PC\\SQLEXPRESS;Initial Catalog=Eli;Integrated Security=True";
 
         //for eden
-        private static string connectionString = "Data Source=Eden-PC\\SQLEXPRESS1;Initial Catalog=Eli;Integrated Security=True";
+       // private static string connectionString = "Data Source=Eden-PC\\SQLEXPRESS1;Initial Catalog=Eli;Integrated Security=True";
 
         //for server
         //private static string connectionString = "Data Source=132.75.252.109;Initial Catalog=es2015;Persist Security Info=True;User ID=es2015;Password=th41pn9";
