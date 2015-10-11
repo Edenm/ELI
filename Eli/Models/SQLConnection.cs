b54d61ce23f0fe563c8e8053c6c4eb6dcec33e47
margulishahar <margulis_shahar@walla.co.il>
2015-10-11 22:45:07 +0300

    update project

--- Eli/Models/SQLConnection.cs
+++ Eli/Models/SQLConnection.cs
@@ -12,9 +12,9 @@ namespace Eli.Models
         //for eden
         //private static string connectionString = "Data Source=Eden-PC\\SQLEXPRESS1;Initial Catalog=Eli;Integrated Security=True";
         //for server
-        private static string connectionString = "Data Source=132.75.252.109;Initial Catalog=es2015;Persist Security Info=True;User ID=es2015;Password=th41pn9";
+      //  private static string connectionString = "Data Source=132.75.252.109;Initial Catalog=es2015;Persist Security Info=True;User ID=es2015;Password=th41pn9";
         //for Hila  -- MISRAD-PC\SQLEXPRESS
-        //private static string connectionString = "Data Source=10.0.0.2\\SQLEXPRESS;Initial Catalog=es2015;Persist Security Info=True;User ID=sa;Password=ism149";
+        private static string connectionString = "Data Source=10.0.0.2\\SQLEXPRESS;Initial Catalog=Eli;Integrated Security=True";
         
         public static DataConnectionDataContext GetDataContextInstance()
         {
