c294621558f528b67f8eef3020fbf39301c447b5
margulishahar <margulis_shahar@walla.co.il>
2015-10-13 19:40:21 +0300

    update button

--- Eli/Models/SQLConnection.cs
+++ Eli/Models/SQLConnection.cs
@@ -8,11 +8,11 @@ namespace Eli.Models
     public class SQLConnection
     {
         //for shahar
-        //private static string connectionString = "Data Source=SHAHAR-PC\\SQLEXPRESS;Initial Catalog=Eli;Integrated Security=True";
+        private static string connectionString = "Data Source=SHAHAR-PC\\SQLEXPRESS;Initial Catalog=Eli;Integrated Security=True";
         //for eden
         //private static string connectionString = "Data Source=Eden-PC\\SQLEXPRESS1;Initial Catalog=Eli;Integrated Security=True";
         //for server
-        private static string connectionString = "Data Source=132.75.252.109;Initial Catalog=es2015;Persist Security Info=True;User ID=es2015;Password=th41pn9";
+        //private static string connectionString = "Data Source=132.75.252.109;Initial Catalog=es2015;Persist Security Info=True;User ID=es2015;Password=th41pn9";
         //for Hila  -- MISRAD-PC\SQLEXPRESS
         //private static string connectionString = "Data Source==MISRAD-PC\\SQLEXPRESS;Initial Catalog=Eli;Integrated Security=True";
         
